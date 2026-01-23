5916ca9022a9f5aa94983d009615b8ed70e495fb
ko25 <ko25@ic.ac.uk>
2026-01-23 19:24:10 +0000

    fix: updated code

--- evaluation_metrics/text_to_image_fidelity/clipscore/image_generation.py
+++ evaluation_metrics/text_to_image_fidelity/clipscore/image_generation.py
@@ -15,9 +15,7 @@ def diffusion_model_pipeline():
 
     try:
         load_dotenv() 
-        hf_token = os.getenv("HF_TOKEN")
-        login(token=hf_token)
-        print("Logged in to Hugging Face Hub.") 
+        
     except Exception as e:
         print(f"Could not log in to Hugging Face Hub: {e}")
 
@@ -28,7 +26,7 @@ def diffusion_model_pipeline():
     pipe = DiffusionPipeline.from_pretrained(
     "AIML-TUDA/stable-diffusion-safe",
     safety_checker=None,
-    require_safety_checker=False,).to(device)
+    require_safety_checker=False).to(device)
 
     return pipe
 
@@ -56,10 +54,10 @@ def generate_forget_images(forget_data_filepath, output_file_directory, sld_conf
     for entry in forget_data:
         unlearnt_image = pipe(prompt = entry['prompt'],**sld_config).images[0]
         # create a file path to save the unlearnt image and save the image
-        image_path = f"{output_file_directory}/unlearnt_image_{entry['id']}.png"
+        image_path = f"{output_file_directory}/unlearnt_image_{entry['image_id']}.png"
         unlearnt_image.save(image_path)
         # the last key pair shows the directory of where the unlearnt images are
-        generated_images.append({"id": entry['id'], "prompt": entry['prompt'], "image_path": image_path})
+        generated_images.append({"image_id": entry['image_id'], "prompt": entry['prompt'], "image_path": image_path})
     # Save the generated images list containing the images filepath
     output_path = f"{output_file_directory}/generated_unlearnt_images.json"
     # write into a json file and create this file
@@ -87,10 +85,10 @@ def generate_normal_images(normal_data_filepath, output_file_directory, pipe = N
         # for normal image generation, we disable SLD completely by using guidance scale = 0
         normal_image = pipe(prompt = entry['prompt'], sld_guidance_scale = 0).images[0]
         # create a file path to save the unlearnt image and save the image
-        image_path = f"{output_file_directory}/normal_image_{entry['id']}.png"
+        image_path = f"{output_file_directory}/normal_image_{entry['image_id']}.png"
         normal_image.save(image_path)
         # the last key pair shows the directory of where the images are
-        generated_images.append({"id": entry['id'], "prompt": entry['prompt'], "image_path": image_path})
+        generated_images.append({"image_id": entry['image_id'], "prompt": entry['prompt'], "image_path": image_path})
     # Save the generated images list containing the images filepath
     output_path = f"{output_file_directory}/generated_normal_images.json"
     # write into a json file and create this file
@@ -102,10 +100,10 @@ def generate_normal_images(normal_data_filepath, output_file_directory, pipe = N
 if __name__ == "__main__":
     pipe = diffusion_model_pipeline()
     # run the SLD unlearning technique with our forget prompts
-    generate_forget_images(forget_data_filepath="data/clipscore/data_forget.json",
-                    output_file_directory="results/clipscore/forget",
+    generate_forget_images(forget_data_filepath="/Users/ongkaisheng/Desktop/ImperialCollege/MSc Group Project/eval-learn/results/clipscore/forget/data_forget.json",
+                    output_file_directory="/Users/ongkaisheng/Desktop/ImperialCollege/MSc Group Project/eval-learn/results/clipscore/forget",
                     sld_config=SafetyConfig.STRONG,
                     pipe=pipe)
-    generate_normal_images(normal_data_filepath="data/clipscore/data_normal.json",
-                    output_file_directory="results/clipscore/normal",
+    generate_normal_images(normal_data_filepath="/Users/ongkaisheng/Desktop/ImperialCollege/MSc Group Project/eval-learn/results/clipscore/normal/data_normal.json",
+                    output_file_directory="/Users/ongkaisheng/Desktop/ImperialCollege/MSc Group Project/eval-learn/results/clipscore/normal",
                     pipe=pipe)
