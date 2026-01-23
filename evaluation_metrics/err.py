2c14ac10757b3699a0170662207f140e22f86756
Nikhil Raghavan <nr125@ic.ac.uk>
2026-01-23 13:02:47 +0000

    feat: beginning to implement err

---
+++ evaluation_metrics/err.py
@@ -0,0 +1,14 @@
+
+class ERR:
+    def __init__(self):
+        
+        pass
+
+    def dataset_loader(self, dataset):
+        # Implement dataset loading logic here
+        pass
+
+    @property
+    def compute(self, model, data_loader):
+        # Implement the ERR computation logic here
+        pass
