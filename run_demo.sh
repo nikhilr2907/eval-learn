05f95e37e5f64f132836b936460ca8a18791fe08
Nikhil Raghavan <nr125@ic.ac.uk>
2026-04-02 17:19:01 +0100

    fix: issues with script

--- run_demo.sh
+++ run_demo.sh
@@ -5,6 +5,7 @@
 #SBATCH --time=02:00:00
 #SBATCH --output=job_%j.log
 export HF_HOME=/vol/bitbucket/nr125/.cache/huggingface
+export TORCH_HOME=/vol/bitbucket/nr125/.cache/torch
 source /vol/bitbucket/nr125/eval_learn_env/bin/activate
 cd /vol/bitbucket/nr125/eval-learn
 python nudity_unlearning_demo.py 2>&1 | grep -v "pthread_setaffinity"
